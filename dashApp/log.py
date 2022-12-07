266df76c69761ca9b81419ecdd517aef50a37173
Gabriel Hrysay <gthrysay@yahoo.com.br>
2022-12-07 19:27:12 -0300

    logging features

---
+++ dashApp/log.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+
+def loggerSetup():
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s (in module %(name)s).')
+        
+        logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s (in module %(name)s).", datefmt="%H:%M:%S")
+        
+
+        log = logging.getLogger()
+
+        """ consoleHandler = logging.StreamHandler(sys.stdout)
+        consoleHandler.setFormatter(formatter)
+        consoleHandler.setLevel(logging.DEBUG)
+        log.addHandler(consoleHandler) """
+
+        fileHandler = logging.FileHandler(filename="log.log", mode='w', encoding=None, delay=False)
+        fileHandler.setFormatter(formatter)
+        fileHandler.setLevel(logging.DEBUG)
+        log.addHandler(fileHandler)
+
+
